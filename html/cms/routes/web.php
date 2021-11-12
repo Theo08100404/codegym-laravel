1bf0fc3cd408f9c02b23de73fb94359f17ddc5bf
Taiyo Aragane <t.m.ny.0404@gmail.com>
2021-11-12 16:11:32 +0900

    a

--- html/cms/routes/web.php
+++ html/cms/routes/web.php
@@ -14,29 +14,22 @@
 use App\Book;
 use Illuminate\Http\Request;
 
-/**
-* 本の一覧表示(books.blade.php)
-*/
-Route::get('/', function () {
-    return view('welcome');
-});
 
-/**
-* 本を追加 
-*/
-Route::post('/books', function (Request $request) {
-    //
-});
+//本のダッシュボードを表示
+Route::get('/', 'BooksController@index');
 
-/**
-* 本を削除 
-*/
-Route::delete('/book/{book}', function (Book $book) {
-    //
-});
+//本を登録
+Route::post('/books', 'BooksController@store');
 
+//更新画面
+Route::post('/booksedit/{books}', 'BooksController@edit');
 
+//更新処理
+Route::post('/books/update', 'BooksController@update');
+
+// 本を削除 
+Route::delete('/book/{book}', 'BooksController@destroy');
+
+//Auth
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
+Route::get('/home', 'BooksController@index')->name('home');
